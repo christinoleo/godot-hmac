d699838f3eddc380f7f84dd924a2bbc289f1b50a
Leonardo Christino <leomilho@gmail.com>
2020-06-10 20:59:23 -0300

    Added dynamodb tutorial

--- addons/summerplaygames.godot-hmac/src/hmac.cpp
+++ addons/summerplaygames.godot-hmac/src/hmac.cpp
@@ -7,11 +7,11 @@ using namespace godot;
 
 void HMAC::_register_methods() {
     register_method("digest", &HMAC::digest);
+    register_method("hexdigest", &HMAC::hexdigest);
 }
 
-String HMAC::digest(PoolByteArray secret, PoolByteArray payload, String type) {
+String HMAC::hexdigest(PoolByteArray secret, PoolByteArray payload, String type) {
     unsigned char localMac[32];
-    char convertedMac[32*2];
 
     const size_t keyLength = secret.size();
     const size_t payloadLength = payload.size();
@@ -36,15 +36,68 @@ String HMAC::digest(PoolByteArray secret, PoolByteArray payload, String type) {
     mbedtls_md_hmac_finish(&ctx, localMac);
     mbedtls_md_free(&ctx);
 
-    for (int i = 0; i < sizeof(localMac); i++) {
-        sprintf(&convertedMac[i*2], "%02x", (int)localMac[i]);   
-    }
+	char* convertedMac = bin2hex(localMac, sizeof(localMac));
 
     delete[] secretBuffer;
     delete[] payloadBuffer;
 
     return String(convertedMac);
 }
+
+PoolByteArray HMAC::digest(PoolByteArray secret, PoolByteArray payload, String type) {
+    unsigned char localMac[32];
+
+    const size_t keyLength = secret.size();
+    const size_t payloadLength = payload.size();
+
+    char* secretBuffer = new char[keyLength]();
+    char* payloadBuffer = new char[payloadLength]();
+
+    for (int i = 0; i < keyLength; i++) {
+        secretBuffer[i] = (char)secret[i];
+    }
+
+    for (int i = 0; i < payloadLength; i++) {
+        payloadBuffer[i] = (char)payload[i];
+    }
+
+    mbedtls_md_context_t ctx;
+    mbedtls_md_init(&ctx);
+    mbedtls_md_type_t mbedType = getMbedType(type.to_lower());
+    mbedtls_md_setup(&ctx, mbedtls_md_info_from_type(mbedType), 1);
+    mbedtls_md_hmac_starts(&ctx, (const unsigned char*) secretBuffer, keyLength);
+    mbedtls_md_hmac_update(&ctx, (const unsigned char *)payloadBuffer, payloadLength);
+    mbedtls_md_hmac_finish(&ctx, localMac);
+    mbedtls_md_free(&ctx);
+
+    PoolByteArray convertedMac = PoolByteArray();
+    for(int i = 0; i < sizeof(localMac); i++){
+        convertedMac.append(localMac[i]);
+    }
+
+    delete[] secretBuffer;
+    delete[] payloadBuffer;
+
+    return convertedMac;
+}
+
+char* HMAC::bin2hex(const unsigned char *bin, size_t len){
+	char *out;
+	size_t  i;
+
+	if (bin == NULL || len == 0)
+		return NULL;
+
+	out = (char*)malloc(len*2+1);
+	for (i=0; i<len; i++) {
+		out[i*2]   = "0123456789abcdef"[bin[i] >> 4];
+		out[i*2+1] = "0123456789abcdef"[bin[i] & 0x0F];
+	}
+	out[len*2] = '\0';
+
+	return out;
+}
+
 
 mbedtls_md_type_t HMAC::getMbedType(String type) {
     if (type == "sha256") { return MBEDTLS_MD_SHA256; }
--- addons/summerplaygames.godot-hmac/src/hmac.hpp
+++ addons/summerplaygames.godot-hmac/src/hmac.hpp
@@ -2,20 +2,22 @@
 #define HMAC_HPP
 
 #include <Godot.hpp>
-#include <Reference.hpp>
+#include <Node.hpp>
 #include <mbedtls/md.h>
 #include <mbedtls/sha256.h>
 
 namespace godot {
-    class HMAC : public Reference {
-        GODOT_CLASS(HMAC, Reference)
+    class HMAC : public Node {
+        GODOT_CLASS(HMAC, Node)
     
     private:
         mbedtls_md_type_t getMbedType(String type);
+	    char* bin2hex(const unsigned char *bin, size_t len);
 
     public:
         static void _register_methods();
-        String digest(PoolByteArray secret, PoolByteArray payload, String type);
+        PoolByteArray digest(PoolByteArray secret, PoolByteArray payload, String type);
+        String hexdigest(PoolByteArray secret, PoolByteArray payload, String type);
 
         HMAC();
         ~HMAC();
